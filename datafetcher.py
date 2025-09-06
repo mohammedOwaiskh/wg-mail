14fcb636c661034e1ee707e52520f9bc900355a6
Mohammed Owais Khan <mohdowaiskh@gmail.com>
2025-09-06 17:35:24 +0530

    UPDATE: datafetcher.py - added transformation logic

--- datafetcher.py
+++ datafetcher.py
@@ -31,12 +31,57 @@ def make_request() -> dict | None:
         logging.error("Error Occured in fetching data")
 
 
+def transform_data(df: pd.DataFrame):
+    rename_cols = {
+        "Privatzimmer_ID": "room_id",
+        "PrivatzimmerArt": "room_type_code",
+        "PrivatzimmerArtName": "room_type",
+        "Status": "status",
+        "FreiVon": "available_from",
+        "WG": "is_share_apartment",
+        "Miete": "cold_rent",
+        "Nebenkosten": "ancillary_costs",
+        "Kaution": "deposit",
+        "Paare": "couple_allowed",
+        "Maenner": "male_only",
+        "Frauen": "female_only",
+        "Auslaender": "foreigner_allowed",
+        "AnzahlZimmer": "no_of_rooms",
+        "Qm": "size_in_sq_mtrs",
+        "Strasse": "street",
+        "Plz": "postal_code",
+        "Ort": "city",
+        "EinrichtungsartName": "furnishing",
+        "VermieterName": "landlord_last_name",
+        "VermieterVorname": "landlord_first_name",
+        "VermieterTelefon": "mobile_number",
+        "VermieterEMail": "email_id",
+    }
+    keep_cols = list(rename_cols.keys())
+
+    new_df = df.drop(df.columns.difference(keep_cols), axis=1).rename(
+        columns=rename_cols
+    )
+
+    new_df.replace(
+        ["leer", "teilmöbliert", "möbliert", "möbl. oder leer"],
+        ["Unfurnished", "Partially Furnished", "Furnished", "Furnished or Unfurnished"],
+    )
+
+    new_df = new_df.replace(
+        ["leer", "teilmöbliert", "möbliert", "möbl. oder leer"],
+        ["Unfurnished", "Partially Furnished", "Furnished", "Furnished or Unfurnished"],
+    )
+
+    return new_df
+
+
 def get_owner_data():
 
     data = make_request()
     if data:
         owner_df = pd.DataFrame(data)
-        owner_df.set_index("Privatzimmer_ID")
+        owner_df = transform_data(owner_df)
 
         print(owner_df)
 
